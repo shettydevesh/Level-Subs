bb695cd320989c7c508515334dc06c64f8372659
Devesh <deveshshetty66@gmail.com>
2023-04-04 18:35:49 +0530

    update

--- data.py
+++ data.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 
 def get_data(file):
-    data = pd.read_excel(file)
+    if file.endswith(".xlsx"):
+        data = pd.read_excel(file)
+    elif file.endswith(".csv"):
+        data = pd.read_csv(file)
     data2 = data[["Type", "Status"]]
     dic = data2.groupby(["Type"]).value_counts()
     monthly_active = dic["MONTHLY"]["active"]
