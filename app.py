f186ee7a284a26d23378d8cc47e91b00fb2ef8c7
Devesh <deveshshetty66@gmail.com>
2023-04-04 18:49:26 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Level", layout="wide")
 def main():
     st.title("Subscription")
     st.header("Upload your Excel File here:")
-    excel_file = st.file_uploader("", key="doc_file", type=['.xlsx'])
+    excel_file = st.file_uploader("", key="doc_file", type=['.csv'])
     if excel_file is not None:
         monthly, weekly, yearly = get_data(excel_file)
         st.subheader(f"Weekly Active: {weekly}")
--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def get_data(file):
-    data = pd.read_excel(file)
+    data = pd.read_csv(file)
     data2 = data[["Type", "Status"]]
     dic = data2.groupby(["Type"]).value_counts()
     monthly_active = dic["MONTHLY"]["active"]
